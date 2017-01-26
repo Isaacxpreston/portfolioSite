99b41712207fbabc8373b8045aa8db5db2c01523
Isaac <isaacxpreston@gmail.com>
2017-01-26 15:31:38 -0800

    menu bar fully integrated

--- client/app/components/menu.js
+++ client/app/components/menu.js
@@ -10,11 +10,13 @@ const Menu = React.createClass({
 		return (
 			<div>
 				<div className={this.props.menuReducer.popup}></div>
-				<div className="popup-option linkedIn"></div>
-				<div className="popup-option facebook"></div>
-				<div className="popup-option github"></div>
-				<div className="popup-option twitter"></div>
-				<div className="popup-option email"></div>
+				<div className={this.props.menuReducer.icons}>
+					<div className="popup-option linkedIn"></div>
+					<div className="popup-option facebook"></div>
+					<div className="popup-option github"></div>
+					<div className="popup-option twitter"></div>
+					<div className="popup-option email"></div>
+				</div>
 
 				<div className="menu">
 					<div className="menu-center"></div>
--- client/app/reducers/menu_reducer.js
+++ client/app/reducers/menu_reducer.js
@@ -1,24 +1,28 @@
 export function menuReducer (state = {
   menu_left: "menu-left",
-  popup: "",
+  popup: "popup hidden",
+  icons: "hidden"
 }, action) {
 	if(action.type === 'MENU_TOGGLE') {
     if(state.menu_left === "menu-left") {
       return {
         menu_left: "menu-left menu-left-selected",
-        popup: "popup"
+        popup: "popup",
+        icons: ""
       }
     } else {
       return {
         menu_left: "menu-left",
-        popup: "popup hidden"
+        popup: "popup hidden",
+        icons: "hidden"
       }
     }
 	}
   if(action.type === 'MENU_CLICKED_AWAY') { //todo - fire this EVERYWHERE.
     return {
       menu_left: "menu-left",
-      popup: "popup hidden"
+      popup: "popup hidden",
+      icons: "hidden"
     }
 	}
 	return state;
